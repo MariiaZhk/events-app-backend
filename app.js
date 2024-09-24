ccb1a30492d198c3f54101e66491314037da7460
Mariia <maria8zhuk@gmail.com>
2024-09-24 15:21:51 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 import eventsRouter from "./routes/eventsRouter.js";
 import participantRouter from "./routes/participantRouter.js";
 
+dotenv.config();
+
 const app = express();
+app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.json());
-app.use("/events", eventsRouter);
-app.use("/participants", participantRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -20,9 +22,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-const PORT = 3000;
-const DB_HOST =
-  "mongodb+srv://MariiaZhk:24121979Zbu@cluster0.qgfgm.mongodb.net/events-registration-app?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
   .connect(DB_HOST)
   .then(() => {
